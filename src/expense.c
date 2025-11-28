1bdd71f34e196ea8d8fc3d05904006ea384b3b02
mdejaz153 <mdejazms365@gmail.com>
2025-11-28 19:46:25 +0530

    Create expense.c

---
+++ src/expense.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "expense.h"
+
+void expenseMenu() {
+    printf("\n--- Expense Tracker Module ---\n");
+    printf("This is a placeholder. Logic will be added later.\n");
+}
