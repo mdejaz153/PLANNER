0399d943693386def9513ee26ee1b0e115b3acfc
mdejaz153 <mdejazms365@gmail.com>
2025-11-28 19:48:09 +0530

    Update reminder.c

--- src/reminder.c
+++ src/reminder.c
@@ -1,1 +1,8 @@
+#include <stdio.h>
+#include "reminder.h"
+
+void reminderMenu() {
+    printf("\n--- Reminder Module ---\n");
+    printf("This is a placeholder. Logic will be added later.\n");
+}
 
