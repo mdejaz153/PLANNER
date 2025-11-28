95b31d31ad9a8ffa6f1b80c0ec46ff4608d65c8d
mdejaz153 <mdejazms365@gmail.com>
2025-11-28 19:47:10 +0530

    Create health.c

---
+++ src/health.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "health.h"
+
+void healthMenu() {
+    printf("\n--- Health Tracker Module ---\n");
+    printf("This is a placeholder. Logic will be added later.\n");
+}
