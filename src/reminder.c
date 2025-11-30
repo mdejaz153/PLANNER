1a5f728bfee3880f872dbb43238b40b7ee0b0e4e
mdejaz153 <mdejazms365@gmail.com>
2025-11-30 10:12:58 +0530

    Update reminder.c

--- src/reminder.c
+++ src/reminder.c
@@ -1,8 +1,67 @@
 #include <stdio.h>
+#include <string.h>
 #include "reminder.h"
 
-void reminderMenu() {
-    printf("\n--- Reminder Module ---\n");
-    printf("This is a placeholder. Logic will be added later.\n");
+void addReminder() {
+    FILE *fp = fopen("reminders.txt", "a");
+    if (!fp) {
+        printf("Error opening reminders file.\n");
+        return;
+    }
+
+    char title[100];
+    char date[20];
+    char time[20];
+
+    printf("Enter reminder title: ");
+    scanf(" %[^\n]s", title);
+
+    printf("Enter date (DD-MM-YYYY): ");
+    scanf("%s", date);
+
+    printf("Enter time (HH:MM): ");
+    scanf("%s", time);
+
+    fprintf(fp, "%s | %s | %s\n", title, date, time);
+    fclose(fp);
+
+    printf("Reminder added successfully!\n");
 }
 
+void viewReminders() {
+    FILE *fp = fopen("reminders.txt", "r");
+    if (!fp) {
+        printf("No reminders found.\n");
+        return;
+    }
+
+    char line[200];
+
+    printf("\n--- Reminder List ---\n");
+
+    while (fgets(line, sizeof(line), fp)) {
+        printf("%s", line);
+    }
+
+    fclose(fp);
+}
+
+void reminderMenu() {
+    int ch;
+
+    do {
+        printf("\n--- Reminder System ---\n");
+        printf("1. Add Reminder\n");
+        printf("2. View All Reminders\n");
+        printf("3. Back\n");
+        printf("Enter choice: ");
+        scanf("%d", &ch);
+
+        switch (ch) {
+            case 1: addReminder(); break;
+            case 2: viewReminders(); break;
+            case 3: return;
+            default: printf("Invalid choice!\n");
+        }
+    } while (ch != 3);
+}
