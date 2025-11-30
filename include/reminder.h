b8dbb7846debfd2672f68c7d4b4d9b2e776959bf
mdejaz153 <mdejazms365@gmail.com>
2025-11-30 12:11:20 +0530

    Documented reminder.h

--- include/reminder.h
+++ include/reminder.h
@@ -1,8 +1,17 @@
+// reminder.h - Header for Reminder Module
+// Purpose: Declares functions for adding and viewing reminders
+// #reminder #tasks #planner
+
 #ifndef REMINDER_H
 #define REMINDER_H
 
+// Menu for reminder operations
 void reminderMenu();
+
+// Adds a new reminder entry
 void addReminder();
+
+// Displays all reminders from reminders.txt
 void viewReminders();
 
 #endif
